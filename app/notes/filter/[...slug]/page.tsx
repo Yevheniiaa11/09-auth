fe73eaa7651f7d20b697eae1ce4aa603c12d942f
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-07-03 22:55:28 +0300

    bugs

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,25 +1,49 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import { fetchNotes } from "../../../../lib/api";
+import { NoteListResponse } from "../../../../types/note";
 import NotesClient from "./Notes.client";
 
-interface NotesProps {
-  params: Promise<{ slug: string[] }>;
+interface NotesPageProps {
+  params: {
+    slug?: string[];
+  };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }
 
 export const revalidate = 60;
 
-export default async function Notes({ params }: NotesProps) {
-  const initialQuery = "";
-  const initialPage = 1;
-  const { slug } = await params;
-  const tag = slug[0] === "all" ? undefined : slug[0];
-  const data = await fetchNotes(initialQuery, initialPage, tag);
+export default async function Notes({ params, searchParams }: NotesPageProps) {
+  const awaitedParams = await Promise.resolve(params);
+  const awaitedSearchParams = await Promise.resolve(searchParams);
+  const initialPage = Number(awaitedSearchParams?.page) || 1;
+  const initialQuery = (awaitedSearchParams?.q as string) || "";
+  const tagFromSlug =
+    awaitedParams.slug?.[0] === "all" ? undefined : awaitedParams.slug?.[0];
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery<NoteListResponse, Error>({
+    queryKey: ["notes", initialQuery, initialPage, tagFromSlug], // Додайте всі параметри, які впливають на фетчинг
+    queryFn: () => fetchNotes(initialQuery, initialPage, tagFromSlug),
+  });
+  const initialData = queryClient.getQueryData<NoteListResponse>([
+    "notes",
+    initialQuery,
+    initialPage,
+    tagFromSlug,
+  ]) || { notes: [], totalPages: 1 };
 
   return (
-    <NotesClient
-      initialQuery={initialQuery}
-      initialPage={initialPage}
-      initialTag={tag}
-      initialData={data}
-    />
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient
+        initialQuery={initialQuery}
+        initialPage={initialPage}
+        initialTag={tagFromSlug}
+        initialData={initialData}
+      />
+    </HydrationBoundary>
   );
 }
