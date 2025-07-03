00588c6669b2d7cc354f81acf64a701b8bc9386c
Yevheniiaa11 <yevheniia.zb@gmail.com>
2025-07-03 23:12:09 +0300

    fixed

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useRouter } from "next/navigation";
-import Modal from "../../../../components/Modal/Modal";
 import NotePreview from "../../../../components/NotePreview/NotePreview";
 import React from "react";
+import Modal from "../../../../components/Modal/Modal";
 interface ModalClientProps {
   noteId: number;
 }
